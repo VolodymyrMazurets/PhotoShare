ee4814a0d62a3b8f14e5e565a93c53a1e25a2b04
VovaV101 <vova0190@bigmir.net>
2024-03-27 22:42:28 +0200

    Change image for post, config.py, post change to crud

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
     POSTGRES_SERVER: str = 'localhost'
     POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str
+    POSTGRES_USER: str = ''
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str = ""
     SMTP_PORT: int = 465
