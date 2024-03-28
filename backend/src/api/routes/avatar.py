2ceb57263671cd82a7182c786394795c0fa27ea2
IvanBosovch <138058545+IvanBosovch@users.noreply.github.com>
2024-03-28 20:47:37 +0200

    added avatar; update profile; update users

---
+++ backend/src/api/routes/avatar.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, UploadFile, File
+from src.crud import users as repository_users
+from src.schemas.users import UserDb
+from sqlalchemy.orm import Session
+from src.models import User
+from src.services.auth import auth_service
+from src.core.db import get_db
+import cloudinary
+import cloudinary.uploader
+from src.core.config import settings
+
+router = APIRouter(prefix="/avatar", tags=["avatar"])
+
+@router.get("/me/", response_model=UserDb)
+async def read_users_me(current_user: User = Depends(auth_service.get_current_user)):
+    return current_user
+
+
+@router.patch('/avatar', response_model=UserDb)
+async def update_avatar_user(file: UploadFile = File(), current_user: User = Depends(auth_service.get_current_user),
+                             db: Session = Depends(get_db)):
+    cloudinary.config(
+        cloud_name=settings.CLOUDINARY_NAME,
+        api_key=settings.CLOUDINARY_API_KEY,
+        api_secret=settings.CLOUDINARY_API_SECRET,
+        secure=True
+    )
+
+    r = cloudinary.uploader.upload(file.file, public_id=f'UsersApp/{current_user.username}', overwrite=True)
+    src_url = cloudinary.CloudinaryImage(f'UsersApp/{current_user.username}')\
+                        .build_url(width=250, height=250, crop='fill', version=r.get('version'))
+    user = await repository_users.update_avatar(current_user.email, src_url, db)
+    return user
---
+++ backend/src/api/routes/profile.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from src.core.db import get_db
+from src.models import User
+from src.schemas.users import UserResponseProfile, UserUpdate
+from src.crud import users as repository_users
+from sqlalchemy.orm import Session
+from src.services.auth import auth_service
+
+
+router = APIRouter(prefix="/profile", tags=["profile"])
+
+@router.get('/user_info', response_model=UserResponseProfile)
+async def user_info(user: str, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    user = await repository_users.get_user_by_username(db, user, current_user)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {'user': user}
+
+
+
+@router.delete('/delete_user', response_model=UserResponseProfile)
+async def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    user = await repository_users.delete_user(user_id, db, current_user)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {'user': user}
+
+
+@router.put('/{user_id}', response_model=UserResponseProfile)
+async def update_user(user_id: int, body: UserUpdate, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    user = await repository_users.update_user(user_id, body, db, current_user)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {'user': user}
+
+
+@router.patch('/update_role', response_model=UserResponseProfile)
+async def update_role(user_id: int, role: str, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    user = await repository_users.update_role(user_id, role, db, current_user)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {'user': user}
