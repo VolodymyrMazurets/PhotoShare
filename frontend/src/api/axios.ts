2314e3640a592d775d4330f48bcac004fa8544b6
Volodymyr Mazurets <volodymyrmazurets@gmail.com>
2024-03-31 12:26:16 +0100

    ✨ add default values for the env frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import { getCookie } from "cookies-next";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_BACKEND_URL}${process.env.NEXT_PUBLIC_API_V1_STR}`;
+axios.defaults.baseURL = `${
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000"
+}${process.env.NEXT_PUBLIC_API_V1_STR || "/api/v1"}`;
 
 axios.interceptors.request.use((config) => {
   const authToken = getCookie("token");
