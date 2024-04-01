842e9c468e2cd20737742876e3f857eedeaac360
Volodymyr Mazurets <volodymyrmazurets@gmail.com>
2024-04-01 12:29:07 +0100

    🔥 remove env config file

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -9,9 +9,6 @@ from pydantic_core import MultiHostUrl
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_ignore_empty=True, extra="ignore"
-    )
     API_V1_STR: str = "/api/v1"
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
